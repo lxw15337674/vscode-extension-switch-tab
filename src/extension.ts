702d44538d12c43582e2eac64f8b355cd12b964c
weiji <weiji@qunhemail.com>
2022-10-13 11:55:41 +0800

    test

--- src/extension.ts
+++ src/extension.ts
@@ -13,16 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('ViewSwitch.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		vscode.commands.executeCommand('editor.action.addCommentLine');
 		vscode.window.showInformationMessage('Hello World from helloWorld!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-
-console.log('test');
 
 
 // This method is called when your extension is deactivated
